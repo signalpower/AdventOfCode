e2232c2f313cd931833af766bf28bb33972e870c
signalpower <1456451+signalpower@users.noreply.github.com>
2025-12-05 15:49:34 +0100

    Updated AoC 2025 day 4 part 1

--- 2025/2025-04-A.py
+++ 2025/2025-04-A.py
@@ -1,29 +1,39 @@
-#import re
+import sys
 
-with open("input_test.txt", "r") as inputfile:
-  inputdata = inputfile.read()
+if lan(sys.argv) < 2:
+  print("Usage python ",sys.argv[0], "<input file>")
+  sys.exit(1)
 
-MIN = 0
-MAX = 99
+try:
+  with open(sys.argv[1], "r") as infile:
+    grid = [line.strip() for line in infile if line.strip()]
+except FileNotFoundError:
+  print("Error: The file was not found.")
+  sys.exit(1)
 
-pos = 50
-matches = 0
-for line in inputdata.split("\n"):
-  if len(line) > 1:
-    dir = line[0]
-    num = int(line[1:])
-    print(pos, " || ", line, "  || Dir: ", dir, " Num:", num)
-    if dir == 'L':
-      pos = pos - num
-      if pos < MIN:
-        pos = MAX - abs(pos)
-    elif dir == 'R':
-      pos = pos + num
-      if pos > MAX:
-        pos = MIN + pos - MAX
-    else:
-      print("Dir neither 'L' or 'R': ", dir)
-    if pos == 0:
-      matches += 1
+CC = '@'
+GH = len(grid)
+GW = len(grid[0])  # if GH > 0 else 0
 
-print("Password is: ", matches)
+ofs = [
+    (-1, -1), ( 0, -1), ( 1, -1),
+    (-1,  0),           ( 1,  0),
+    (-1,  1), ( 0, 1),  ( 1,  1)
+]
+
+p = 0
+
+for y in range(len(grid)):
+  for x in range(len(grid[y])):
+    if grid[y][x] == CC:
+      nc = 0
+      for dx, dy in ofs:
+        nx = x + dx
+        ny = y + dy
+        if (0 <= nx < GW) and (0 <= ny < GH):
+          if grid[ny][nx] == CC:
+            nc += 1
+      if nc < 4:
+        p += 1
+
+print("Answer is: ", p)
